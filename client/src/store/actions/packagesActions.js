8ea186185c01862e1b5f520fe612927dfc97aa1a
Yair Revivo <yrevivo@attentigroup.com>
2019-11-05 14:24:36 +0200

    add null defence

--- client/src/store/actions/packagesActions.js
+++ client/src/store/actions/packagesActions.js
@@ -5,7 +5,7 @@ import { GET_PACKAGES_BY_NAME } from './constants';
 export const getPackagesByName = (packageDetails) => dispatch => {
   return fetch('/api/packages/' + packageDetails.packageName + '/' + packageDetails.packageVersion)
     .then(res => res.json())
-    .then(packages => dispatch({ type: GET_PACKAGES_BY_NAME, payload: packages !== undefined ? packages.dependencies || packages.devdependencies : null }))
+    .then(packages => dispatch({ type: GET_PACKAGES_BY_NAME, payload: packages.dependencies || packages.devdependencies || [] }))
 }
 
 
